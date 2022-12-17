33600b48d126a63139bb06ef09afb4aa6f9f75af
Roduan Kareem Aldeen <37983260+RoduanKD@users.noreply.github.com>
2022-12-17 15:07:22 +0300

    flip script and template tags

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,46 @@
+<template>
+  <header>
+    <h1 id="name">JS 101</h1>
+  </header>
+
+  <section class="container">
+    <form @submit.prevent="addToList">
+      <h1>Add User</h1>
+      <div v-if="message" class="msg" :class="messageClass" v-text="message"></div>
+      <div>
+        <label for="name">Name:</label>
+        <input type="text" name="first_name" v-model.trim="firstName">
+      </div>
+      <div>
+        <label for="email">Email:</label>
+        <input type="email" name="email" v-model.trim="email">
+      </div>
+      <input class="btn" type="submit" value="Submit">
+    </form>
+    <div>
+      <h3>Preview</h3>
+      {{ preview }}
+      {{ preview }}
+      {{ preview }}
+    </div>
+    <hr>
+    <button @click="removeFirstUser" class="mx-2">Remove First user</button>
+    <button @click="emptyUsersList" class="mx-2">Clear users</button>
+    <button @click="removeLastUser" class="mx-2">Remove Last user</button>
+    <button @click="removeUserAt" class="mx-2">Remove user at</button>
+    <input type="number" min="1" :max="usersList.length" placeholder="index" v-model="selectedIndex">
+    <button @click="loadUsersFromApi" class="mx-2">Load Users from API</button>
+
+    <input type="search" placeholder="Name" v-model.trim="searchValue">
+
+    <ul class="items">
+      <li v-for="(user, i) in filteredList" class="item" :key="user.id">#{{ user.id }} {{ user.name }}: {{ user.email }}
+        <span class="icon" @click="removeUser(i)">x</span>
+      </li>
+    </ul>
+  </section>
+</template>
+
 <script>
 export default {
   data() {
@@ -99,47 +142,3 @@ export default {
   }
 }
 </script>
-
-<template>
-  <header>
-    <h1 id="name">JS 101</h1>
-  </header>
-
-  <section class="container">
-    <form @submit.prevent="addToList">
-      <h1>Add User</h1>
-      <div v-if="message" class="msg" :class="messageClass" v-text="message"></div>
-      <div>
-        <label for="name">Name:</label>
-        <input type="text" name="first_name" v-model.trim="firstName">
-      </div>
-      <div>
-        <label for="email">Email:</label>
-        <input type="email" name="email" v-model.trim="email">
-      </div>
-      <input class="btn" type="submit" value="Submit">
-    </form>
-    <div>
-      <h3>Preview</h3>
-      {{ preview }}
-      {{ preview }}
-      {{ preview }}
-    </div>
-    <hr>
-    <button @click="removeFirstUser" class="mx-2">Remove First user</button>
-    <button @click="emptyUsersList" class="mx-2">Clear users</button>
-    <button @click="removeLastUser" class="mx-2">Remove Last user</button>
-    <button @click="removeUserAt" class="mx-2">Remove user at</button>
-    <input type="number" min="1" :max="usersList.length" placeholder="index" v-model="selectedIndex">
-    <button @click="loadUsersFromApi" class="mx-2">Load Users from API</button>
-
-    <input type="search" placeholder="Name" v-model.trim="searchValue">
-
-    <ul class="items">
-      <li v-for="(user, i) in filteredList" class="item" :key="user.id">#{{ user.id }} {{ user.name }}: {{ user.email }}
-        <span class="icon" @click="removeUser(i)">x</span>
-      </li>
-    </ul>
-  </section>
-</template>
-
