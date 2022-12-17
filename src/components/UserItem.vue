5ade3c83cd34a5e5fcad83e4f845f3e865f0c5fb
Roduan Kareem Aldeen <37983260+RoduanKD@users.noreply.github.com>
2022-12-17 15:24:13 +0300

    create UserItem component with three props

---
+++ src/components/UserItem.vue
@@ -0,0 +1,11 @@
+<template>
+  <li class="item">#{{ id }} {{ name }}: {{ email }}
+    <span class="icon" @click="removeUser(i)">x</span>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['id', 'name', 'email'],
+}
+</script>
